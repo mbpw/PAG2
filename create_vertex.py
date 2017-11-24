30f0012b683b46f097561269e52745d2640fe2ac
Mateusz <mateusz.bialek@o2.pl>
2017-11-24 13:38:02 +0100

    skrypt z vertexami

---
+++ create_vertex.py
@@ -0,0 +1,25 @@
+import arcpy
+
+infc = arcpy.GetParameterAsText(0)
+
+# Enter for loop for each feature
+#
+for row in arcpy.da.SearchCursor(infc, ["OID@", "SHAPE@"]):
+    # Print the current line ID
+
+    print("Feature {0}:".format(row[0]))
+
+    #Set start point
+    startpt = row[1].firstPoint
+
+    #Set Start coordinates
+    startx = startpt.X
+    starty = startpt.Y
+
+    #Set end point
+    endpt = row[1].lastPoint
+
+    #Set End coordinates
+    endx = endpt.X
+    endy = endpt.Y
+	#cośdodałem
